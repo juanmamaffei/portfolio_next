bcf9f29e876ae0b71d917b2e320d5ced23062c95
Juanma <juanmamaffei@gmail.com>
2021-12-27 00:12:07 -0300

    A little bit of styling...

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
 
       </Head>
-      <div className="flex flex-col justify-between min-h-screen">
+      <div className="flex flex-col min-h-screen">
           
         <div className="p-10 min-h-80 items-center bg-white">
           <div className="text-center">
@@ -24,40 +24,39 @@ export default function Home() {
           </div>        
         </div>      
         
-        <div className="bg-stone-900 w-full flex-auto p-5">
-          <div className="flex flex-col text-purple-200 items-center justify-between h-max">
+        <div className="flex-auto text-purple-200 bg-stone-900 w-full p-5 flex flex-col auto-cols-max items-center">
+          <div className="flex-auto">
           <h2 className="text-3xl font-bold my-4">About</h2>
-          <div className="">
-          <p>Hi! I'm a fullstack dev, content creator and teacher. Here, you will find some works.</p>
-          <p>I divide it in:</p>
-          <ul>
-            <li><strong>Projects:</strong> full development projects for me and for clients. </li>
-            <li><strong>Other works:</strong> pieces of another works or using other technologies.</li>
-            <li><strong>Multimedia:</strong> audiovisual, transmedia or social media productions.</li>
-          </ul>
-          </div>
-          <div className="my-2 flex flex-row flex-nowrap justify-center items-center text-center">
-            
-            <Link href="/projects">
-              <a className="m-2 btn-primary">Projects</a>
-            </Link>
-            <Link href="/samples">
-              <a className="m-2 btn-primary">Other works</a>
-            </Link>
-            <Link href="/multimedia">
-              <a className="m-2 btn-primary">Multimedia</a>
-            </Link>
 
           </div>
+          <div className="flex-auto text-lg">
+          <p>Hi! I'm a fullstack dev, content creator and teacher. Here, you will find some works.</p>
+          <p>I divide it in:</p>
+          <ul className="m-2">
+          <Link href="/projects">
+            <li className="list"><strong>Projects:</strong> full development projects for me and for clients. </li></Link>
+            <Link href="/samples">
+            <li className="list"><strong>Other works:</strong> pieces of another works or using other technologies.</li>
+            </Link>
+            <Link href="/multimedia">
+            <li className="list"><strong>Multimedia:</strong> audiovisual, transmedia or social media productions.</li>
+            </Link>
+          </ul>
+          </div>
         </div>
-        </div>
-        <div className="w-full h-full py-5 flex flex-col items-center">
-          <h2 className="text-2xl font-bold text-violet-50">Contact</h2>
-          <div className="align-middle text-violet-100">
-          <a href="">
-            <Image src="/images/linkedin.png" height="30px" width="30px" />
+        <div className="w-full h-full py-5 flex flex-col items-center font-black">
+
+          <div className="text-white py-3 px-5 animate-pulse hover:animate-none hover:text-xl flex flex-row items-center">
+            <div className="mr-2">
+              <a href="https://www.linkedin.com/in/juanmamaffei/" target="_blank">
+              <Image src="/images/linkedin.png" height="30px" width="30px" />
+            </a>
+            </div>
+            <div className=""> 
+              <a href="https://www.linkedin.com/in/juanmamaffei/" target="_blank">
             Find me in LinkedIn
             </a>
+            </div>
           </div>
         </div>
       </div>
